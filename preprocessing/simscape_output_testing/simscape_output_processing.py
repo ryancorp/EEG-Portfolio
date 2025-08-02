b24f50f34c6fd6a540b7f3d43e73062b00420bf0
Ryan Corp <ryanjamescorp@gmail.com>
2025-08-01 19:07:50 -0700

    Updated to emulate ADC data stream.

--- preprocessing/simscape_output_testing/simscape_output_processing.py
+++ preprocessing/simscape_output_testing/simscape_output_processing.py
@@ -1,21 +1,42 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import time
+from collections import deque
+
+#Simulate ADC SPS
+SAMPLE_RATE_HZ = 1000 #Simscape samples every 1ms real ADC will be every 2.5ms
+SAMPLE_INTERVAL = 1.0 / SAMPLE_RATE_HZ
+
+SLIDING_BUFFER_SECONDS = 5
+SLIDING_BUFFER_SIZE = SLIDING_BUFFER_SECONDS * SAMPLE_RATE_HZ
+
+CHUNK_DURATION_SECONDS = 1 #Amount of data sent per step size
+CHUNK_SIZE = int(SAMPLE_RATE_HZ * CHUNK_DURATION_SECONDS)
+
+PAD_DURATION_SECONDS = 0.25 #Seconds to Pad around Chunk Sent
+PAD_SIZE = int(SAMPLE_RATE_HZ * PAD_DURATION_SECONDS)
+
+STEP_DURATION_SECONDS = 0.1 #How often samples are sent
+STEP_SIZE = int(SAMPLE_RATE_HZ * STEP_DURATION_SECONDS)
+
+CSV_FILE = 'simscape_output.csv'
 
 #Load CSV
 data = pd.read_csv('simscape_output.csv')
 eeg_signal = data.iloc[:, 1].values
-fs = 1000 #ADS1115 can sample at 860Hz maximum, Simscape is at 1000Hz
+
 
 #Bandpass filter 1-50 Hz
 from scipy.signal import butter, filtfilt
 
 def bandpass_filter(signal, low, high, fs, order=4):
-    nyq = 0.5*fs
-    b, a = butter(order,[low/nyq, high/nyq], btype='band')
-    return filtfilt(b, a, signal) #research filtfilt
-
-filtered_signal = bandpass_filter(eeg_signal, 1, 50, fs) #This removes DC offset, and the system is now centerd around 0V
+    nyq = 0.5 * fs
+    b, a = butter(order, [low / nyq, high / nyq], btype='band')
+    
+    return filtfilt(b, a, signal)
 
 #Artifact rejection
 def interpolate_artifacts(signal, threshold=0.252/2): #ADC clips at 0.253V and 0V
@@ -34,34 +55,62 @@ def interpolate_artifacts(signal, threshold=0.252/2): #ADC clips at 0.253V and 0
     
     return signal
 
-clean_signal = interpolate_artifacts(filtered_signal)
-
-def chunk_signal(signal, chunk_size):
-    return np.array([signal[i:i + chunk_size]
-                    for i in range(0, len(signal), chunk_size)
-                    if i + chunk_size <= len(signal)])
-                    
-chunk_size = int(fs*1) # 1-second chunks
-chunks = chunk_signal(clean_signal, chunk_size)
-
+#Chunking
 def serialize_chunk(chunk):
     return ','.join(map(str, chunk)) + '\n'
-
-for chunk in chunks:
-    serialized = serialize_chunk(chunk)
-    print(serialized)
-    
-    
-import matplotlib.pyplot as plt
-for i, chunk in enumerate(chunks):
-    start_time = i #seconds
-    time_axis = np.linspace(start_time + 0.001, start_time+1.0, chunk_size)
+                    
+#Plotting
+def plot_chunk(chunk, sample_counter, chunk_index):
+    start_time = (sample_counter - CHUNK_SIZE - PAD_SIZE)/SAMPLE_RATE_HZ 
+    end_time = (sample_counter - PAD_SIZE)/SAMPLE_RATE_HZ 
+    time_axis = np.linspace(start_time, end_time, CHUNK_SIZE)
     
     plt.figure(figsize=(6, 2))
     plt.plot(time_axis, chunk)
-    plt.title(f'Chunk {i+1} | Time: {start_time + 0.001:.3f}s to {start_time + 1.0:.3f}s')
+    plt.title(f'Chunk {chunk_index + 1} | Time: {start_time:.3f}s to {end_time:.3f}s')
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+
+#Pretending there is a data stream like with an ADC
+def stream_csv(CSV_FILE):
+    buffer = deque(maxlen=SLIDING_BUFFER_SIZE)
+    chunk_index = 0
+    sample_counter = 0
+    
+    with open(CSV_FILE, 'r') as file:
+        reader = csv.reader(file)
+        
+        start_time = time.perf_counter()
+        
+        for i, row in enumerate(reader):
+            current_time = time.perf_counter()
+            elapsed  = current_time - start_time
+            target_time = i * SAMPLE_INTERVAL
+            sleep_time = target_time - elapsed
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+            
+            try:
+                sample = float(row[1])
+            except ValueError:
+                continue
+            
+            buffer.append(sample)
+            sample_counter += 1
+            
+            #Sliding buffer that starts after 1s and grows to 5s
+            if len(buffer) >= (CHUNK_SIZE + 2 * PAD_SIZE) and (sample_counter % STEP_SIZE) == 0:
+                full_buffer = np.array(buffer)
+                filtered = bandpass_filter(full_buffer, 1, 50, SAMPLE_RATE_HZ)
+                clean = interpolate_artifacts(filtered)
+
+                latest_chunk = clean[-(CHUNK_SIZE+PAD_SIZE):-PAD_SIZE]  #Take sample with PAD_SIZE padding from end of data
+                
+                serialized = serialize_chunk(clean) #Pretend output for sending to MATLAB
+                plot_chunk(latest_chunk, sample_counter, chunk_index)
+                chunk_index += 1
+
+stream_csv(CSV_FILE) #Run process
